936cd4d8957a2b21057cbe35422f8bc6d282d56d
Wieland Brendel <wieland.brendel@posteo.de>
2019-01-05 14:20:32 +0100

    fixed weight path

--- foolbox_model.py
+++ foolbox_model.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 from model import make_model
 import foolbox
+import inspect
+import os
 
 TEMPERATURE = 100
-MODEL_PATH = 'models/distilled'
 
 def create():
     sess = tf.Session().__enter__()
     _input = tf.placeholder(tf.float32, (1, 28, 28, 1))
-
+    
+    local_path = os.path.dirname(os.path.realpath(inspect.stack()[0][1]))
+    MODEL_PATH = os.path.join(local_path, 'models/distilled')
     model = make_model(MODEL_PATH)
     _logits = model(_input)/TEMPERATURE
 
